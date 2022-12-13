daa0720d6f1bf013daa7ed2a001e6e28afbf3374
Anonymous User <65858862+miabonardi@users.noreply.github.com>
2022-12-13 16:50:25 -0500

    add only enable documents

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.FddAnnualRenewal',
-      version='2.9',
+      version='2.1.1',
       description=('Fdd annual renewal'),
       long_description='# docassemble.FddAnnualRenewal\r\n\r\nFdd annual renewal\r\n\r\n## Author\r\n\r\nauthor@example.com\r\n\r\n',
       long_description_content_type='text/markdown',
